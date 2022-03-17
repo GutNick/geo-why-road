8e0e8fff552a6cf78eff919aa3ee231445855d88
Nikita Gutman <chelyabinsk.it@gmail.com>
2022-03-18 00:45:31 +0500

    Правки по ревью

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const visible_blocks = document.querySelectorAll('.experience__hidden');
 window.onscroll = function () {
   visible_blocks.forEach((elem) => {
     if (elem.getBoundingClientRect().top <= height / 1.5) {
+      if (elem.classList.contains('experience__dot')) {
+        elem.classList.add('experience__dots_flashing');
+      }
       elem.classList.add('experience__visible');
     }
   })
